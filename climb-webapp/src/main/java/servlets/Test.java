7ea6bd247141695c2934a070ccc02006f4e8a56a
Dorian <fenestre.dorian@free.fr>
2020-04-11 21:04:20 +0200

    test servlet/jsp

--- climb-webapp/src/main/java/servlets/Test.java
+++ climb-webapp/src/main/java/servlets/Test.java
@@ -1,12 +1,13 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet("/Test")
 public class Test extends HttpServlet {
     public Test() {
         super();
